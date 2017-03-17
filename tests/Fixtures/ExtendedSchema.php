84dd2c8a99550771d78c4ad844b1641bacaf0284
initvector <ryan.p@vanillaforums.com>
2017-03-17 13:30:51 -0400

    Add ability to pass constructor arguments with Schema::Parse

--- tests/Fixtures/ExtendedSchema.php
+++ tests/Fixtures/ExtendedSchema.php
@@ -13,4 +13,29 @@ use Garden\Schema\Schema;
  * A basic subclass of Schema.
  */
 class ExtendedSchema extends Schema {
+
+    /** @var string */
+    public $controller;
+
+    /** @var string */
+    public $method;
+
+    /** @var string */
+    public $type;
+
+    /**
+     * ExtendedSchema constructor.
+     *
+     * @param array $schema
+     * @param string $controller
+     * @param string $method
+     * @param string $type
+     */
+    public function __construct(array $schema = [], $controller = null, $method = null, $type = null) {
+        parent::__construct($schema);
+
+        $this->controller = $controller;
+        $this->method = $method;
+        $this->type = $type;
+    }
 }
