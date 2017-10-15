56add97bade6f757214ea66355ff04736ade6778
Todd Burry <todd@vanillaforums.com>
2017-10-15 10:32:43 -0400

    Add some basic multiple type unit tests

---
+++ tests/MultipleTypesTest.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * @author Todd Burry <todd@vanillaforums.com>
+ * @copyright 2009-2017 Vanilla Forums Inc.
+ * @license MIT
+ */
+
+namespace Garden\Schema\Tests;
+use Garden\Schema\Schema;
+
+/**
+ * Tests for schemas where the type property is multiple types.
+ */
+class MultipleTypesTest extends AbstractSchemaTest {
+    /**
+     * Provide test data for basic multiple type tests.
+     *
+     * @return array Returns a data provider.
+     */
+    public function provideBasicMultipleTypeTests() {
+        $r = [
+            ['integer array 1', 123],
+            ['integer array 2', [1]],
+        ];
+
+        return array_column($r, null, 0);
+    }
+
+    /**
+     * Basic multiple type tests.
+     *
+     * @param string $types A space delimited string of type names.
+     * @param mixed $value A value to test.
+     * @param mixed $expected The expected valid value.
+     * @dataProvider provideBasicMultipleTypeTests
+     */
+    public function testBasicMultipleTypes($types, $value, $expected = null) {
+        $types = array_filter(explode(' ', $types), function ($v) {
+            return !is_numeric($v);
+        });
+
+        $sch = new Schema([
+            'type' => $types
+        ]);
+
+        $expected = $expected === null ? $value : $expected;
+
+        $valid = $sch->validate($value);
+        if (is_array($expected)) {
+            $this->assertEquals($expected, $valid);
+        } else {
+            $this->assertSame($expected, $valid);
+        }
+    }
+}
